8d78ecb023a2165e3b22a31d321ca5e8d2bb8a69
G1Daniel <danielguesh8888@gmail.com>
2023-12-13 10:11:27 +0300

    database connection using psycopg2

--- script/db_connection.py
+++ script/db_connection.py
@@ -4,6 +4,7 @@ from psycopg2 import sql
 from sqlalchemy import create_engine
 import pandas as pd
 
+#---------------using sqlalchemy--------
 def db_connection_sqlalchemy():
     # database connection using sqlalchemy
     database_name='telecom'
@@ -23,3 +24,15 @@ def db_read_table_sqlalchemy(engine,table_name):
     sql_query=f'SELECT * FROM {table_name}'
     df = pd.read_sql(sql_query, con= engine)
     return df    
+
+#----------------using psycopg-------------
+def db_connection_psycopg():
+     # database connection using psycopg2
+    dbconn=psycopg2.connect(
+        dbname="telecom",
+        user="postgres",
+        password="admin",
+        host="localhost",
+        port="5432"
+    )
+    return dbconn   
